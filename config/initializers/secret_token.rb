c8db64a1205733dc7c009d2c79cb19bd4fbe08ec
vanesagh <vanesag@chalmers.se>
2013-11-19 20:02:07 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaggingwiApp::Application.config.secret_key_base = '3630a176718b93c29e8369f930842011e085d51c095d0c2f34517a0900007afb7cf52fb4d8afaf383ab392e4ce238a973dd94f2a59acedb2d574b194069d21f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TaggingwiApp::Application.config.session_store :cookie_store, key: '_taggingwi_app_session'
